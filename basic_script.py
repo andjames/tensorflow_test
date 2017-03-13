2cbff2f3d4098b1ef601544c99124126dc0baed8
Andrew James <andrew.james@wsj.com>
2017-03-13 17:49:05 +0800

    first commit

---
+++ basic_script.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+a = tf.add(1, 2,)
+b = tf.mul(a, 3)
+c = tf.add(4, 5,)
+d = tf.mul(c, 6,)
+e = tf.mul(4, 5,)
+f = tf.div(c, 6,)
+g = tf.add(b, d)
+h = tf.mul(g, f)
+
+with tf.Session() as sess:
+    writer = tf.summary.FileWriter("output", sess.graph)
+    print(sess.run(h))
+    writer.close()
